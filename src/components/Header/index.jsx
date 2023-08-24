e1d7f728e72108323f4c490cb68ffb585370826b
Crodic <113219638+Crodic@users.noreply.github.com>
2023-08-24 18:07:26 +0700

    responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,16 +3,30 @@ import {
     Box,
     Button,
     Divider,
+    Drawer,
     IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
     Toolbar,
     Typography,
 } from "@mui/material";
 import Logo from "../../assets/images/came.svg";
-import { DarkMode, LightMode } from "@mui/icons-material";
+import {
+    Close,
+    DarkMode,
+    LibraryAdd,
+    LightMode,
+    Menu,
+} from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { stateApp } from "../../redux/selector";
 import { changeMode } from "../../redux/slice/appSlice";
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 const ListMenu = ["About", "Work", "Testimonials", "Contact"];
 
@@ -25,53 +39,155 @@ const CustomButton = styled(Button)(({ theme }) => ({
 }));
 
 const Header = () => {
+    const [drawer, setDrawer] = useState(false);
     const { mode } = useSelector(stateApp);
     const dispatch = useDispatch();
     const handleChangeMode = () => {
         dispatch(changeMode(`${mode === "light" ? "dark" : "light"}`));
     };
+    const toggleDrawer = (_, open) => (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+        setDrawer(open);
+    };
     return (
-        <AppBar position="static">
-            <Toolbar sx={{ width: "89%", margin: "0 auto", padding: 0 }}>
-                {/**Logo */}
+        <>
+            <AppBar position="static">
+                <Toolbar sx={{ width: "89%", margin: "0 auto", padding: 0 }}>
+                    {/**Logo */}
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: 2,
+                        }}
+                        flexGrow={1}
+                    >
+                        <img
+                            src={Logo}
+                            alt="logo-came"
+                            style={{ width: "40px", height: "40px" }}
+                        />
+                        <Typography variant="h6">CAME</Typography>
+                    </Box>
+
+                    {/**Action */}
+                    <Box sx={{ display: { xs: "none", lg: "flex" }, gap: 2 }}>
+                        {ListMenu.map((item, index) => {
+                            return (
+                                <Button
+                                    key={index}
+                                    variant="text"
+                                    sx={{ color: "text.primary" }}
+                                    href="#about"
+                                >
+                                    {item}
+                                </Button>
+                            );
+                        })}
+                        <Divider orientation="vertical" flexItem />
+                        <IconButton onClick={handleChangeMode}>
+                            {mode === "light" ? <LightMode /> : <DarkMode />}
+                        </IconButton>
+                        <CustomButton variant="contained">
+                            Download CV
+                        </CustomButton>
+                    </Box>
+                    <Box sx={{ display: { xs: "block", lg: "none" } }}>
+                        <IconButton onClick={toggleDrawer(drawer, true)}>
+                            <Menu />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor="right"
+                open={drawer}
+                onClose={toggleDrawer(drawer, false)}
+            >
                 <Box
                     sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: 2,
+                        width: "325px",
                     }}
-                    flexGrow={1}
+                    role="presentation"
                 >
-                    <img
-                        src={Logo}
-                        alt="logo-came"
-                        style={{ width: "40px", height: "40px" }}
-                    />
-                    <Typography variant="h6">CAME</Typography>
-                </Box>
-
-                {/**Action */}
-                <Box sx={{ display: { xs: "none", lg: "flex" }, gap: 2 }}>
-                    {ListMenu.map((item, index) => {
-                        return (
-                            <Button
-                                key={index}
-                                variant="text"
-                                sx={{ color: "text.primary" }}
-                                href="#about"
+                    <List>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                paddingX: "32px",
+                                paddingY: "8px",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: 2,
+                                }}
+                                flexGrow={1}
                             >
-                                {item}
-                            </Button>
-                        );
-                    })}
-                    <Divider orientation="vertical" flexItem />
-                    <IconButton onClick={handleChangeMode}>
-                        {mode === "light" ? <LightMode /> : <DarkMode />}
-                    </IconButton>
-                    <CustomButton variant="contained">Download CV</CustomButton>
+                                <img
+                                    src={Logo}
+                                    alt="logo-came"
+                                    style={{ width: "40px", height: "40px" }}
+                                />
+                                <Typography variant="h6">CAME</Typography>
+                            </Box>
+                            <Box>
+                                <IconButton
+                                    sx={{ textAlign: "right", width: "auto" }}
+                                    onClick={toggleDrawer(drawer, false)}
+                                    onKeyDown={toggleDrawer(drawer, false)}
+                                >
+                                    <Close />
+                                </IconButton>
+                            </Box>
+                        </Box>
+                        {ListMenu.map((item, index) => {
+                            return (
+                                <ListItem
+                                    key={index}
+                                    onClick={toggleDrawer(drawer, false)}
+                                    onKeyDown={toggleDrawer(drawer, false)}
+                                >
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <LibraryAdd />
+                                        </ListItemIcon>
+                                        <ListItemText>{item}</ListItemText>
+                                    </ListItemButton>
+                                </ListItem>
+                            );
+                        })}
+                        <Divider variant="middle" />
+                        <ListItem onClick={handleChangeMode}>
+                            <ListItemButton>
+                                <ListItemText>Switch Theme</ListItemText>
+                                <ListItemIcon>
+                                    {mode === "light" ? (
+                                        <LightMode />
+                                    ) : (
+                                        <DarkMode />
+                                    )}
+                                </ListItemIcon>
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem>
+                            <CustomButton variant="contained" fullWidth>
+                                Download CV
+                            </CustomButton>
+                        </ListItem>
+                    </List>
                 </Box>
-            </Toolbar>
-        </AppBar>
+            </Drawer>
+        </>
     );
 };
 
